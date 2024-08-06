fc551bfe6022681c887e49417ab9510485480c7d
Nicholas Cher Kwok Hui <nicholas@infiniwell.com>
2024-08-06 16:21:00 +0200

    Finalise UI

--- TodoList/TodoListApp.swift
+++ TodoList/TodoListApp.swift
@@ -14,7 +14,7 @@ struct TodoListApp: App {
     
     var body: some Scene {
         WindowGroup {
-            NavigationView {
+            NavigationStack {
                 ListView()
             }
             .environmentObject(listViewModel)
